48e38b07124fc8866c94a394abf2444316b30ebb
Tony Trinh <tony.w.trinh3@gmail.com>
2019-01-15 00:14:48 -0800

    mailer
    mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,7 +8,21 @@ const keys = require ('../config/keys');
 //so remember react when we are looking at this
 //can treat this like a normal react component in terms of syntax and set up
 class Mailer extends helper.Mail {
+    //constructor is called automatically when you use a new keyword
+    //gives us an opportunity to do some amount of setup or initialization inside of the class instance
+    //constructor function is going to be called automatically for us. any arguments that we use when we use the new keyword will be providedas arguments to the constructor function
+    //we can call the mailer in the future with a new subject, new list of recipients, and some different contents
+    constructor({subject, recipients}, content){
+        super();
 
+        //who this email appears to be sent from
+        //people using emaily service to send our survey
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html',content);
+        this.recipients = this.formatAddresses(recipients);
+
+    }
 
 }
 
