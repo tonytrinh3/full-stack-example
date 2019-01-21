550f214e5cd8b92a190b0e0b87b45ba07d8485d6
Tony Trinh <tony.w.trinh3@gmail.com>
2019-01-21 11:52:20 -0800

    survey

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,14 @@
 // SurveyField contains logic to render a single label and text input
-
+//lec 149
 import React from 'react';
-
-export default () => {
+//or export default ({ input, label }) for taking only the input object of props
+export default (props) => {
+    
+    //console.log(props.input); this shows every single change that happens through Fields
     return(
         <div>
-            <input />
+            <label>{props.label}</label>
+            <input {...props.input}/>
         </div>
     );
 
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,21 +1,43 @@
 //SurveyForm shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 //Field component that we can use to show any type of different input or any type of different HTML element that will somehow collect input from our user
 //thought like a swiss army knife, lect 148
 import { reduxForm, Field } from 'redux-form';
+import {Link} from 'react-router-dom';
 import SurveyField from './SurveyField';
+
+//lec 152
+const FIELDS = [
+    {label: "Survey Title",name: "title" },
+    {label: "Survey Line", name: "subject"},
+    {label: "Email Body", name: "body" },
+    {label: "Recipient List", name: "emails" }
+];
+
+
 
 //lec 147
 class SurveyForm extends Component{
     
     //lect 149
     renderFields(){
-        return (
-            {/* Field strength is wiring up all the different event handlers for watching changes to input*/}
-            <div>
-                <Field type = 'text' name = "title" component = {SurveyField} />
-            </div>
-        );
+        //map function from lodash
+        return _.map(FIELDS, field =>{
+            //key property needs to be unique 
+            return <Field key = {field.name} component = {SurveyField} type = "text" label = {field.label} name = {field.name} />
+        });
+
+        //return ( 
+            //<div>
+                /* Field strength is wiring up all the different event handlers for watching changes to input
+                <Field label = "Survey Title" type = 'text' name = "title" component = {SurveyField} />
+                <Field label = "Survey Line" type = 'text' name = "subject" component = {SurveyField} />
+                <Field label = "Email Body" type = 'text' name = "body" component = {SurveyField} />
+                <Field label = "Recipient List" type = 'text' name = "emails" component = {SurveyField} />
+                */
+            //</div>
+        //);
     }
     
     
@@ -31,7 +53,13 @@ class SurveyForm extends Component{
                 <form 
                 onSubmit = {this.props.handleSubmit(values => console.log(values))}>
                     {this.renderFields()}
-                    <button type ="submit">Submit</button>
+                    <Link to="/surveys" className= "red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type ="submit" className = "teal btn-flat right white-text">
+                        Next
+                        <i className = 'material-icons right'>done</i>
+                    </button>
                 </form>
             </div>
 
