48e2800c1348358402268fe7dd65006b148d6505
Tony Trinh <tony.w.trinh3@gmail.com>
2019-01-26 22:22:27 -0800

    awef
    aewf

--- sendgrid_webhook.js
+++ sendgrid_webhook.js
@@ -1,6 +1,9 @@
 var localtunnel = require('localtunnel');
-localtunnel(5000, { subdomain: <awefawefewhehehwhwh> }, function(err, tunnel) {
+localtunnel(5000, { subdomain: "awefawefewhehehwhwh" }, function(err, tunnel) {
   console.log('LT running')
 });
 
 //http://awefawefewhehehwhwh.localtunnel.me/api/surveys/webhooks
+
+//"webhook": "forever sendgrid_webhook.js"
+//"webhook": "ngrok http 5000"
